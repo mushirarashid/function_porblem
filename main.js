1938e0aa6d528c374e572c735ccee5a6b5e5c039
mushirarashid <mushirarashid2003@gmail.com>
2024-04-26 16:56:21 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+var number = [2, 3, 52, 26, 57];
+
+var sliceAns = number.slice(1, 3);
+console.log(sliceAns);
+
+// function
+
+// function funName() {
+//   console.log("go go first");
+// }
+// funName();
+
+// perameter
+
+// function fun(perameterName) {
+//   console.log("go go first");
+//   console.log(perameterName);
+// }
+// fun(2000);
+
+// function fun(money) {
+//   console.log("go go first");
+//   console.log(money);
+// }
+// fun(50);
+
+// function functionName(peraName) {
+//   console.log("Pls give me tk :", peraName);
+// }
+
+// functionName(500000);
+
+// function functionName(peraName2) {
+//   console.log("Pls give me tk :", peraName2);
+// }
+// var peraInputVar = "ami prbo na";
+// functionName(peraInputVar);
+
+// sum into function
+
+// function functionSum(number1, number2) {
+//   console.log(number1, number2);
+//   var innerSum = number1 + number2;
+//   console.log(innerSum);
+// }
+
+// functionSum(500, 200);
+
+// function functionOdd(odd1, odd2, odd3, odd4) {
+//   var oddSum = odd1 + odd2 + odd3 + odd4;
+//   console.log(oddSum);
+// }
+// functionOdd(15, 20, 3, 102);
+
+// function bringCake(money) {
+//   var perPrice = 10;
+//   var getCake = money / perPrice;
+//   return getCake;
+// }
+// var returnPrice = bringCake(50);
+// console.log(returnPrice);
+
+// function googler(man) {
+//   var all_googlers = 182502;
+//   var count_of_bd_googler = man / all_googlers;
+//   return count_of_bd_googler;
+// }
+// var googler_from_bd = 100;
+// var returnGoogler = googler(googler_from_bd);
+// console.log(returnGoogler);
+
+// function getAverage(assignment1, assignment2, assignment3) {
+//   var total_mark_of_assignment = assignment1 + assignment2 + assignment3;
+//   var average = total_mark_of_assignment / 3;
+//   return average;
+// }
+// var assignment_1_mark = 95;
+// var assignment_2_mark = 79;
+// var assignment_3_mark = 100;
+// var returnAverage = getAverage(
+//   assignment_1_mark,
+//   assignment_2_mark,
+//   assignment_3_mark
+// );
+
+// console.log("Final result of average :", returnAverage);
+
+// objects start
+
+var firstObjects = {
+  myName: "mushira",
+  age: 20,
+  gender: "girl",
+};
+console.log(firstObjects);
+
+console.log(firstObjects.age);
+
+firstObjects.myName = "Rashid";
+console.log(firstObjects);
